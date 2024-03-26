82a01f9d24ec86d4bcea68527a5ce35e24d7e5f9
Paolo Pedrigal <paolopedrigal@gmail.com>
2024-03-26 02:03:29 -0700

    feat: starting to implement card deck swiping

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,54 +1,136 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  DimensionValue,
+  ImageSourcePropType,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Card from "@components/Card";
-import { DrawerItem } from "@react-navigation/drawer";
+import Swiper from "react-native-deck-swiper";
+import { useRef } from "react";
+
+// const styles = StyleSheet.create({
+//   home: {
+//     backgroundColor: "#0C0B44",
+//   },
+// });
 
 const styles = StyleSheet.create({
-  home: {
-    backgroundColor: "#0C0B44",
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF",
+  },
+  card: {
+    flex: 1,
+    width: "100%",
+    height: 500,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
   },
 });
 
 const data = [
   {
     id: "1",
-    backgroundColorStyle: "#7A7ACA",
+    backgroundColor: "#7A7ACA",
     text: "Who was the celebrity you've been the closest to?",
     authorText: "Question of the Day",
+    isAuthorBold: true,
   },
   {
     id: "2",
-    backgroundColorStyle: "#2A2A6B",
+    backgroundColor: "#2A2A6B",
     text: "Bruno Mars in Las Vegas!",
+    image: require("@assets/images/vegas.jpg"),
     authorText: "maikaroni",
+    authorImage: require("@assets/images/test-pfp.jpg"),
+    isAuthorBold: false,
   },
   {
     id: "3",
-    backgroundColorStyle: "#24245E",
+    backgroundColor: "#24245E",
     text: "Pitbull at work",
     authorText: "johnmiranda",
+    authorImage: require("@assets/images/test-pfp.jpg"),
+    isAuthorBold: false,
   },
   {
     id: "4",
-    backgroundColorStyle: "#131254",
-    text: "Jeremy Lin",
+    backgroundColor: "#131254",
+    // text: "Jeremy Lin bruv",
+    image: require("@assets/images/linsanity.jpg"),
     authorText: "buansanity",
+    authorImage: require("@assets/images/test-pfp.jpg"),
+    isAuthorBold: false,
   },
 ];
 
+interface CardProps {
+  backgroundColor: string;
+  width?: DimensionValue | undefined;
+  text?: string;
+  image?: ImageSourcePropType; // TODO: Not sure if this should be a string or some image prop
+  authorText: string;
+  isAuthorBold: boolean;
+  authorImage?: ImageSourcePropType;
+}
+
 export default function HomeScreen() {
+  const swiper = useRef<Swiper<CardProps>>(null);
+  const press = () => {
+    if (swiper.current) {
+      console.log("Swiping back");
+      swiper.current.swipeBack();
+    }
+  };
   return (
-    <View style={styles.home}>
-      <FlatList
-        data={data}
-        renderItem={({ item }) => (
-          <Card
-            backgroundColorStyle={item.backgroundColorStyle}
-            text={item.text}
-            authorText={item.authorText}
-          />
-        )}
-        keyExtractor={(item) => item.id}
-      />
-    </View>
+    <Swiper
+      ref={swiper}
+      cards={data}
+      cardIndex={0} // dont know about this
+      // containerStyle={{ alignItems: "center", justifyContent: "center" }}
+      backgroundColor={"#0C0B44"}
+      cardVerticalMargin={30}
+      cardHorizontalMargin={12}
+      renderCard={(card) => (
+        <Card
+          backgroundColor={card.backgroundColor}
+          text={card.text}
+          image={card?.image}
+          authorText={card.authorText}
+          authorImage={card.authorImage}
+          isAuthorBold={card.isAuthorBold}
+        />
+      )}
+      stackSize={4}
+      stackSeparation={0}
+      // disableTopSwipe
+      // disableBottomSwipe
+      // goBackToPreviousCardOnSwipeLeft
+      disableLeftSwipe
+      disableRightSwipe
+      disableBottomSwipe
+      // goBackToPreviousCardOnSwipeBottom
+      showSecondCard={true}
+      infinite
+      childrenOnTop
+      swipeBackCard
+      stackAnimationTension={1}
+      // onSwipedBottom={press}
+    >
+      <Pressable style={{ position: "absolute", bottom: 30 }} onPress={press}>
+        <Text style={{ color: "white" }}>Go back</Text>
+      </Pressable>
+    </Swiper>
   );
 }
